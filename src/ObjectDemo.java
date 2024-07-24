888c1dd4e518c73d39e9744db000a5f1323a6220
Prathamesh Patil <prathavidhi1234@gmail.com>
2024-07-24 16:50:43 +0530

    vi text editor Update Added git.ignore and removed bin/ from the staging

--- src/ObjectDemo.java
+++ src/ObjectDemo.java
@@ -6,6 +6,6 @@ String info = "Hello";
 public static void main(String args[]) {
 	ObjectDemo o = new ObjectDemo();
 	System.out.println("values of o's are "+ o.val +":"+ o.info);
-	System.out.println("hello word");
+	System.out.println("hello word");                        
 }
 }
