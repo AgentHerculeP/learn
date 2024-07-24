9500d30fd5836b5c6330236db91faba2e5223cf5
Prathamesh Patil <prathavidhi1234@gmail.com>
2024-07-24 15:57:20 +0530

    new changes

--- src/ObjectDemo.java
+++ src/ObjectDemo.java
@@ -5,5 +5,7 @@ String info = "Hello";
 
 public static void main(String args[]) {
 	ObjectDemo o = new ObjectDemo();
-	System.out.println("values of o's are "+ o.val +":"+ o.info);}
+	System.out.println("values of o's are "+ o.val +":"+ o.info);
+	System.out.println("hello word");
+}
 }
